5bcbdbb4510a2f91c7ba13adc799a045395bf77b
bhushan-borole <37565807+bhushan-borole@users.noreply.github.com>
2018-11-01 16:01:46 +0530

    18B updated

--- 18B_grades.c
+++ 18B_grades.c
@@ -1,61 +1,40 @@
-#include<stdio.h> 
-#include<conio.h> 
+#include <stdio.h>
+#include <conio.h>
 
-void main() 
-{
-    int marks=0;
-    char grade=' ';
-    clrscr(); 
-    printf("\nenter total marks: ");
-    scanf("%d",&marks);
-    asm {
-        mov ax,marks
-        cmp ax,80
-	    jg l1
-        jb nc
-    }
-    nc: asm {
-        cmp ax,60
-	    jg l2
-        je l3
-        jb nc2
-    }
-    nc2: asm {
-        cmp ax,40
-	    jg l4
-        jb nc3
-    }
-    nc3: asm {
-        cmp ax,39
-	    jg l6
-    }
-    l1: asm {
-	    mov grade,'a'
-	    jmp e1
-    }
-    l2: asm {
-	    cmp ax,79
-	    jb l3
-    }
-    l3: asm {
-        mov grade,'b'
-        jmp e1
-    }
-    l4: asm {
-        cmp ax,59
-        jb l5
-    }
-    l5: asm {
-        mov grade,'c'
-        jmp e1
-    }
-    l6: asm {
-	    mov grade,'f'
-        jmp e1
-    }
-    e1: asm {
+void main(){
+	int marks;
+	char grade;
+	clrscr();
+	printf("Enter Marks: ");
+	scanf("%d", &marks);
+	asm{
+		mov ax, marks
+		cmp ax, 80
+		jg L1
+		cmp ax, 60
+		jg L2
+		cmp ax, 40
+		jg L3
+		jmp L4
+	}
+	L1: asm{
+		mov grade, 'A'
+		jmp exit
+	}
+	L2: asm{
+		mov grade, 'B'
+		jmp exit
+	}
+	L3: asm{
+		mov grade, 'C'
+		jmp exit
+	}
+	L4: asm{
+		mov grade, 'F'
+	}
+	exit: asm{
 
-    }
-    printf("\ngrade is %c",grade);
-    getch(); 
+	}
+	printf("Grade: %c", grade);
+	getch();
 }
